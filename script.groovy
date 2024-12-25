1cbe6c2230a14354580c568be1a0d3347b1e2559
michaelanunda <101601723+michaelanunda@users.noreply.github.com>
2024-12-25 00:57:52 -0600

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -1,5 +1,15 @@
-def buildApp() {
+def buildJar() {
     echo 'building the application...'
+    sh 'mvn package'
+}
+
+def buildImage() {
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+                        sh 'docker build -t uba31/demo-app:jma-2.0 .'
+                        sh 'echo $PASSWORD | docker login -u $USERNAME --password-stdin'
+                        sh 'docker push uba31/demo-app:jma-2.0'
+                    }
 }
 
 def testApp() {
@@ -8,7 +18,6 @@ def testApp() {
 
 def deployApp() {
     echo 'deploying the application...'
-    echo "deploying version ${params.VERSION}"
 }
 
 return this
